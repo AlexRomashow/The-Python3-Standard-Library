e114258058276f59b19ca73a91b72ec582360fc2
Alex Romashow <alexromashow7@gmail.com>
2022-04-20 14:43:48 +0300

    add contextlib_contextmanager_decorator.py

---
+++ Algorihtms/contextlib_contextmanager_decorator.py
@@ -0,0 +1,31 @@
+import contextlib
+
+@contextlib.contextmanager
+def make_context():
+    print('entering')
+    try:
+        # Передается управление, а не значение, поскольку в случае
+        # использования менеджера контекста в качестве декоратора
+        # любое возвращенное значение остается недоступным
+        yield
+    except RuntimeError as err:
+        print('ERROR:', err)
+    finally:
+        print('exiting')
+
+@make_context()
+def normal():
+    print('inside with statement')
+
+@make_context()
+def throw_error(err):
+    raise err
+
+print('Normal:')
+normal()
+
+print('\nHandled error:')
+throw_error(RuntimeError('showing example of handing an error'))
+
+print('\nUnhandled error:')
+throw_error(ValueError('this exception is not handled'))
