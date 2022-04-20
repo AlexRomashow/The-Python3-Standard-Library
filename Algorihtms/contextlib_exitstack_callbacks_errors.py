06b820393453791e071e489691cb68c86e9318b7
Alex Romashow <alexromashow7@gmail.com>
2022-04-20 14:45:04 +0300

    add contextlib_exitstack_callbacks_errors.py

---
+++ Algorihtms/contextlib_exitstack_callbacks_errors.py
@@ -0,0 +1,12 @@
+import contextlib
+
+def callback(*args, **kwargs):
+    print('closing callback({}, {})'.format(args, kwargs))
+
+try:
+    with contextlib.ExitStack() as stack:
+        stack.callback(callback, 'arg1', 'arg2')
+        stack.callback(callback, arg3='val3')
+        raise RuntimeError('thrown error')
+except RuntimeError as err:
+    print('ERROR: {}'.format(err))
