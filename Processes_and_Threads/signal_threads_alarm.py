2890d0dbdc8fa2884160e6779e5f7f723512404c
AlexRomashow <alexromashow7@gmail.com>
2022-06-08 16:03:41 +0300

    add signal_threads_alarm.py

---
+++ Processes_and_Threads/signal_threads_alarm.py
@@ -0,0 +1,40 @@
+import signal
+import time
+import threading
+
+def signal_handler(num, stack):
+    print(time.ctime(), 'Alarm in',
+        threading.currentThread().name)
+
+signal.signal(signal.SIGALRM, signal_handler)
+
+def use_alarm():
+    t_name = threading.currentThread().name
+    print(time.ctime(), 'Setting alarm in', t_name)
+    signal.alarm(1)
+    print(time.ctime(), 'Sleeping in', t_name)
+    time.sleep(3)
+    print(time.ctime(), 'Done with sleep in', t_name)
+
+# Запустить поток, который не будет получать сигналы
+alarm_thread = threading.Thread(
+    target=use_alarm,
+    name='alarm_thread',
+)
+alarm_thread.start()
+time.sleep(0.1)
+
+# Ожидать появления сигнала (этого никогда не произойдет!)
+print(time.ctime(), 'Waiting for', alarm_thread.name)
+alarm_thread.join()
+
+print(time.ctime(), 'Exiting normally')
+
+
+
+
+
+
+
+
+
