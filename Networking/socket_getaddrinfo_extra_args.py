f849cbc188fde6f2b0d077019a6debe27e70682d
AlexRomashow <alexromashow7@gmail.com>
2022-08-02 20:08:13 +0300

    add socket_getaddrinfo_extra_args.py

---
+++ Networking/socket_getaddrinfo_extra_args.py
@@ -0,0 +1,34 @@
+import socket
+
+def get_constants(prefix):
+    """Создать словарь, сопоставляющий константы
+    модуля socket c их именами.
+    """
+    return {
+        getattr(socket, n): n
+        for n in dir(socket)
+        if n.startswith(prefix)
+    }
+
+families = get_constants('AF_')
+types = get_constants('SOCK_')
+protocols = get_constants('IPPROTO_')
+
+responses = socket.getaddrinfo(
+    host='www.python.org',
+    port='http',
+    family=socket.AF_INET,
+    type=socket.SOCK_STREAM,
+    proto=socket.IPPROTO_TCP,
+    flags=socket.AI_CANONNAME,
+)
+
+for response in responses:
+    # Распаковка кортежа ответа
+    family, socktype, proto, canonname, sockaddr = response
+    print('Family        :', families[family])
+    print('Type          :', types[socktype])
+    print('Protocol      :', protocols[proto])
+    print('Canonical name:', canonname)
+    print('Socket address:', sockaddr)
+    print()
